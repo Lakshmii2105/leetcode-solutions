0ae519a55f7256ba490050beec6d1485f14e2ecd
Lakshmii2105 <sarukollalakshmi2005@gmail.com>
2026-01-20 19:03:48 +0530

    Time: 1 ms (100%), Space: 47 MB (15.79%) - LeetHub

---
+++ 3314-construct-the-minimum-bitwise-array-i/3314-construct-the-minimum-bitwise-array-i.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int[] minBitwiseArray(List<Integer> nums) {
+        int n = nums.size();
+        int[] ans = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            int x = nums.get(i);
+            if ((x%2) == 0) {
+                ans[i] = -1;
+            } else {
+ int trailingOnes = Integer.numberOfTrailingZeros(~x);
+                ans[i] = x - (1 << (trailingOnes - 1));
+            }
+        }
+        return ans;
+    }
+}

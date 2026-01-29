f05656f06bb45efde689066f5afe360c274d28e0
Lakshmii2105 <sarukollalakshmi2005@gmail.com>
2026-01-29 18:13:29 +0530

    Time: 30 ms (92.88%), Space: 48.9 MB (12.52%) - LeetHub

---
+++ 0575-distribute-candies/0575-distribute-candies.java
@@ -0,0 +1,10 @@
+ import java.util.HashSet;
+ class Solution {
+    public int distributeCandies(int[] candyType) {
+     HashSet<Integer> arr= new HashSet<>();
+     for(int i:candyType){
+        arr.add(i);
+     }
+return Math.min(arr.size(), candyType.length / 2);
+    }
+ }

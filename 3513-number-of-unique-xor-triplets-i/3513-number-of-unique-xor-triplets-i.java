79c8ef148eb7b147bc0cb84c70d26cebe660f660
Sarukolla Lakshmi <sarukollalakshmi2005@gmail.com>
2026-07-23 18:50:37 +0530

    Time: 1 ms (51.02%), Space: 125.3 MB (77.55%) - LeetHub

---
+++ 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int uniqueXorTriplets(int[] nums) {
+     HashSet<Integer> set = new HashSet<>();
+        int n = nums.length;
+        if (n == 1) {
+         return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        int bits = 32 - Integer.numberOfLeadingZeros(n);
+        return 1 << bits;   
+    }
+}

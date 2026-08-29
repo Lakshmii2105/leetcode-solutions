0ba5748707393e762524bf06b4727911169c014a
Sarukolla Lakshmi <sarukollalakshmi2005@gmail.com>
2026-08-29 23:15:22 +0530

    Time: 1 ms (78.43%), Space: 42.7 MB (21.52%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isHappy(int n) {
+         HashSet<Integer> seen = new HashSet<>();
+        while (n != 1 && !seen.contains(n)) {
+            seen.add(n);
+        int sum=0;
+        int rem;
+     while(n>0){
+        rem=n%10;
+        sum=sum+rem*rem;
+        n/=10;
+     }
+     n=sum;
+     }
+     return n==1;   
+    }
+}

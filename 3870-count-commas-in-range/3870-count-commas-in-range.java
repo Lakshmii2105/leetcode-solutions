a4e8210133c8a763e1ff792306bad2c800840a57
Sarukolla Lakshmi <sarukollalakshmi2005@gmail.com>
2026-09-08 18:34:21 +0530

    Time: 1 ms (83.65%), Space: 42.8 MB (8.32%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,14 +1,9 @@
 class Solution {
     public int countCommas(int n) {
-       int answer=0;
-       for(int i=1;i<=n;i++) {
-        if(i<1000){
-            answer=answer+0;
+    
+        if(n<1000){
+            return 0;
         }
-        else{
-            answer=answer+1;
-        }
-       }
-       return answer;
+       return n-1000+1;
     }
 }

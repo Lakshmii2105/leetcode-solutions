a3255acdf7b7d1602623317efa77decacf05a02d
Sarukolla Lakshmi <sarukollalakshmi2005@gmail.com>
2026-09-22 18:37:31 +0530

    Time: 40 ms (5.84%), Space: 46 MB (45.35%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPowerOfThree(int n) {
+      if(n<=0)
+      return false;
+    for(int x=0;x<=100;x++){
+        if(Math.pow(3,x)==n){
+            return true;
+        }
+    }
+    return false;
+}
+}
